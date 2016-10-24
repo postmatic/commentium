ad6e6d812914c52473cf4c5e024e0de7f0520301
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2016-10-24 11:53:50 -0700

    Add the extended post list

---
+++ source/Lists/Posts/Post.php
@@ -0,0 +1,133 @@
+<?php
+namespace Postmatic\Commentium\Lists\Posts;
+
+use Prompt_Post;
+
+class Post extends Prompt_Post {
+
+	/** @var string */
+	protected static $digest_meta_key = '_prompt_in_digest';
+	/** @var string */
+	protected static $exclude_meta_key = '_prompt_exclude_from_digests';
+	/** @var string */
+	protected static $comment_digest_callback_id_meta_key = '_prompt_comment_digest_callback_id';
+	/** @var string */
+	protected static $digested_comments_date_gmt_meta_key = '_prompt_digested_comments_date_gmt';
+
+	/**
+	 * Record that this post has been included in a digest mailing.
+	 *
+	 * @since 0.3.0
+	 * @param int $digest_plan_id
+	 * @return $this
+	 */
+	public function add_sent_digest( $digest_plan_id ) {
+		add_post_meta( $this->id, static::$digest_meta_key, $digest_plan_id );
+		return $this;
+	}
+
+	/**
+	 * Toggle the flag to exclude the post from digests.
+	 * @since 0.3.0
+	 * @param bool $exclude
+	 * @return $this
+	 */
+	public function set_exclude_from_digests( $exclude = true ) {
+		if ( $exclude ) {
+			update_post_meta( $this->id, static::$exclude_meta_key, true );
+		} else {
+			delete_post_meta( $this->id, static::$exclude_meta_key );
+		}
+		return $this;
+	}
+
+	/**
+	 * Whether the post is excluded from digests.
+	 * @since 0.3.0
+	 * @return bool
+	 */
+	public function get_exclude_from_digests() {
+		return (bool) get_post_meta( $this->id, static::$exclude_meta_key, true );
+	}
+
+	/**
+	 * @since 0.4.0
+	 * @return bool
+	 */
+	public function in_digest_discussion_mode() {
+		return ( 0 !== $this->get_flood_control_comment_id() );
+	}
+
+	/**
+	 * @since 0.4.0
+	 * @param int|null $id Null to remove
+	 * @return $this
+	 */
+	public function set_comment_digest_callback_id( $id = null ) {
+		if ( $id ) {
+			update_post_meta( $this->id, static::$comment_digest_callback_id_meta_key, intval( $id ) );
+		} else {
+			delete_post_meta( $this->id, static::$comment_digest_callback_id_meta_key );
+		}
+		return $this;
+	}
+
+	/**
+	 * @since 0.4.0
+	 * @return int|null Null if none set.
+	 */
+	public function get_comment_digest_callback_id() {
+		$callback_id = get_post_meta( $this->id, static::$comment_digest_callback_id_meta_key, true );
+		return $callback_id ? intval( $callback_id ) : null;
+	}
+
+	/**
+	 * Get the last time comments were digested in GMT.
+	 *
+	 * Defaults to the GMT post date.
+	 *
+	 * @since 0.4.0
+	 * @return string
+	 */
+	public function get_digested_comments_date_gmt() {
+		$date = get_post_meta( $this->id, static::$digested_comments_date_gmt_meta_key, true );
+		$date = $date ?: $this->get_wp_post()->post_date_gmt;
+		return $date;
+	}
+
+	/**
+	 * Set the last time comments were digested in GMT.
+	 *
+	 * @since 0.4.0
+	 * @param string $date
+	 * @return $this
+	 */
+	public function set_digested_comments_date_gmt( $date ) {
+		update_post_meta( $this->id, static::$digested_comments_date_gmt_meta_key, $date );
+		return $this;
+	}
+
+	/**
+	 * Get an array of date clauses to select undigested comments on this post.
+	 * @since 0.4.0
+	 * @return array
+	 */
+	public function undigested_comments_date_clauses() {
+		return array(
+			array( 'column' => 'comment_date_gmt', 'after' => $this->get_digested_comments_date_gmt() )
+		);
+	}
+
+	/**
+	 * Get a meta query clause to select posts that have not yet been sent out in a digest.
+	 *
+	 * @since 0.4.0
+	 * @return array
+	 */
+	public static function include_in_digest_meta_clauses() {
+		return array(
+			array( 'key' => self::$digest_meta_key, 'compare' => 'NOT EXISTS', ),
+			array( 'key' => self::$exclude_meta_key, 'compare' => 'NOT EXISTS', ),
+		);
+	}
+}
---
+++ tests/phpunit/Lists/Posts/Post.php
@@ -0,0 +1,90 @@
+<?php
+namespace Postmatic\Commentium\Unit_Tests\Lists\Posts;
+
+use Postmatic\Commentium\Lists;
+
+use WP_UnitTestCase;
+
+class Post extends WP_UnitTestCase {
+
+	function test_digest_exclusion() {
+		$sent_post = new Lists\Posts\Post( static::factory()->post->create() );
+		$exclude_post = new Lists\Posts\Post( static::factory()->post->create() );
+		$unsent_post_id = static::factory()->post->create();
+
+		$check_posts = get_posts();
+
+		$this->assertCount( 3, $check_posts );
+
+		$sent_post->add_sent_digest( 1 );
+		$exclude_post->set_exclude_from_digests( true );
+
+		$check_posts = get_posts( array( 'meta_query' => array( Lists\Posts\Post::include_in_digest_meta_clauses() ) ) );
+
+		$this->assertCount( 1, $check_posts );
+		$this->assertEquals( $unsent_post_id, $check_posts[0]->ID, 'Expected the unsent post.' );
+	}
+
+	function test_digest_discussion_mode_default() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+		$this->assertFalse( $post_list->in_digest_discussion_mode(), 'Expected post NOT to be in digest discussion mode.' );
+	}
+
+	function test_digest_discussion_mode_flood() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+
+		$post_list->set_flood_control_comment_id( 1 );
+
+		$this->assertTrue( $post_list->in_digest_discussion_mode(), 'Expected post to be in digest discussion mode.' );
+	}
+
+	function test_comment_digest_callback_id_default() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+
+		$this->assertNull(
+			$post_list->get_comment_digest_callback_id(),
+			'Expected no callback ID by default.'
+		);
+	}
+
+	function test_set_comment_digest_callback_id() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+
+		$post_list->set_comment_digest_callback_id( 3 );
+
+		$this->assertEquals(
+			3,
+			$post_list->get_comment_digest_callback_id(),
+			'Expected the comment digest callback id that was set.'
+		);
+	}
+
+	function test_digested_comments_date_gmt_default() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+
+		$this->assertEquals( $post_list->get_wp_post()->post_date_gmt, $post_list->get_digested_comments_date_gmt() );
+	}
+
+	function test_set_digested_comments_date_gmt() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+
+		$test_date = get_gmt_from_date( 'yesterday' );
+
+		$post_list->set_digested_comments_date_gmt( $test_date );
+
+		$this->assertEquals( $test_date, $post_list->get_digested_comments_date_gmt() );
+	}
+
+	function test_undigested_comments_date_clauses() {
+		$post_list = new Lists\Posts\Post( static::factory()->post->create() );
+
+		$digested_date = get_gmt_from_date( '3 hours ago' );
+
+		$post_list->set_digested_comments_date_gmt( $digested_date );
+
+		$clauses = $post_list->undigested_comments_date_clauses();
+
+		$this->assertEquals( 'comment_date_gmt', $clauses[0]['column'], 'Expected digested comment GMT date column.' );
+		$this->assertEquals( $digested_date, $clauses[0]['after'], 'Expected digested date in after clause.' );
+	}
+}
