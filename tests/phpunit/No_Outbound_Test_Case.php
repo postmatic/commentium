d63b37e88800dfca6a684e5435a3782da3a94fe7
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2016-11-02 09:34:26 -0700

    Add no outbound test case

---
+++ tests/phpunit/No_Outbound_Test_Case.php
@@ -0,0 +1,25 @@
+<?php
+namespace Postmatic\Commentium\Unit_Tests;
+
+use WP_UnitTestCase;
+
+class No_Outbound_Test_Case extends WP_UnitTestCase {
+
+	protected $remove_outbound_hooks = true;
+
+	function setUp() {
+		parent::setUp();
+
+		if ( $this->remove_outbound_hooks ) {
+			$this->remove_outbound_hooks();
+		}
+	}
+
+	function remove_outbound_hooks() {
+		remove_action( 'transition_post_status',        array( 'Prompt_Outbound_Handling', 'action_transition_post_status' ) );
+		remove_action( 'wp_insert_comment',             array( 'Prompt_Outbound_Handling', 'action_wp_insert_comment' ) );
+		remove_action( 'transition_comment_status',     array( 'Prompt_Outbound_Handling', 'action_transition_comment_status' ) );
+		remove_filter( 'comment_moderation_recipients', array( 'Prompt_Outbound_Handling', 'filter_comment_moderation_recipients' ) );
+	}
+
+}
