c3ecfb011c22dd4a622b7408a90d06ad92de0016
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2016-11-02 15:35:10 -0700

    Prevent outbounds in factory test

--- tests/phpunit/Filters/Factory.php
+++ tests/phpunit/Filters/Factory.php
@@ -1,13 +1,13 @@
 <?php
 namespace Postmatic\Commentium\Unit_Tests\Filters;
 
+use Postmatic\Commentium\Unit_Tests\No_Outbound_Test_Case;
 use Postmatic\Commentium\Filters;
 use Prompt_Comment_Flood_Controller;
 use Prompt_Core;
 use Prompt_Enum_Email_Transports;
-use WP_UnitTestCase;
 
-class Factory extends WP_UnitTestCase {
+class Factory extends No_Outbound_Test_Case {
 
 	public function test_make_default_comment_flood_controller() {
 		$comment = static::factory()->comment->create_and_get();
