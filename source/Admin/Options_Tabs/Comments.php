3e0a0061f99e11831f652ffa1728874c9fff3737
Jason Lemieux <jason@gopostmatic.com>
2017-05-16 12:31:13 +0000

    UI language

--- source/Admin/Options_Tabs/Comments.php
+++ source/Admin/Options_Tabs/Comments.php
@@ -84,7 +84,7 @@ class Comments extends Prompt_Admin_Comment_Options_Tab {
 
 		$entries[2]['title'] = __( 'Comment digests', 'commentium' );
 		$entries[2]['desc'] = __(
-			'How many comments during a 14 hour period will trigger comment digests to be sent instead of individual comments? There is a minimum of 3.',
+			'How many comments during a 14 hour period will trigger comment digests to be sent instead of individual comments? If you would like to send only direct replies and a daily digest of new comment activity set this to 1.',
 			'commentium'
 		) . html( 'p',
 			__(
